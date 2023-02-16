97a2b2aa0f6c2066271f07d0c6564276b9076b2b
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-02-16 18:40:24 -0300

    update

--- app/validacao.js
+++ app/validacao.js
@@ -7,8 +7,8 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         <h3> Pressione o botão para jogar novamente </h3>
         <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `;
-        
-    document.body.style.backgroundColor = "black";
+
+   
   }
   if (chuteForInvalido(numero)) {
     elementoChute.innerHTML += "<div>Valor inválido</div>";
